e481a1e8b3525d1e0ea8a498c8f9a99ce0c81000
kavya159 <72870853+kavya159@users.noreply.github.com>
2020-12-18 20:04:45 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,20 @@
+class Bird  extends Parent{
+    constructor(x, y) {
+      super(x,y,50,50)
+     
+      this.image=loadImage("bird.png")
+      
+    };
+    display(){
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+     super.display();
+  
+     
+      
+      
+      
+    };
+  };
+  
---
+++ Box.js
@@ -0,0 +1,9 @@
+class Box  extends Parent {
+  constructor(x, y, width, height) {
+  super(x,y,width,height) 
+    
+   this.image=loadImage("wood1.png") 
+    
+  }
+  
+};
